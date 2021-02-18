286641ed5df144417c91d7da269b014e7833083a
vict000r <wiolearn@gmail.com>
2021-02-18 12:20:20 +0100

    sounds

---
+++ drum-player.js
@@ -0,0 +1,41 @@
+
+
+function play1() {
+    var x = document.getElementById("boom");
+    x.play();
+}
+
+function play2() {
+    var x = document.getElementById("clap");
+    x.play();
+}
+
+function play3() {
+    var x = document.getElementById("hihat");
+    x.play();
+}
+
+function play4() {
+    var x = document.getElementById("boom");
+    x.play();
+}
+
+function play5() {
+    var x = document.getElementById("boom");
+    x.play();
+}
+
+function play6() {
+    var x = document.getElementById("boom");
+    x.play();
+}
+
+function play7() {
+    var x = document.getElementById("boom");
+    x.play();
+}
+
+function play8() {
+    var x = document.getElementById("boom");
+    x.play();
+}
